12e6fbd6ef31ffd9aacbbfc9e103eca5aad6c172
nabedge <nabedge@gmail.com>
2014-02-26 18:41:36 +0900

    fix path

--- createzip.sh
+++ createzip.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-zip -j ./target/mixer2-${MIXER2_VERSION}.zip target/dependency/*.jar
+zip -j ./target/mixer2-${MIXER2_VERSION}.zip ./target/dependency/*.jar
 
-# org.apache.maven.plugins:maven-dependency-plugin:2.8:copy-dependencies
 
